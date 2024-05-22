8f14d897d26baa530cef79723e445cfaf033ab9b
d4lan <dylanperezwalker@gmail.com>
2024-05-21 21:49:22 -0700

    Add Comments

--- infra/tf_backend/main.tf
+++ infra/tf_backend/main.tf
@@ -2,22 +2,20 @@ locals {
   name_suffix = "_tf_state"
 }
 
+# Create S3 Bucket for TF Backend
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.project_name + local.name_suffix
 }
-
 resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.terraform_state.id
   acl    = "private"
 }
-
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
     status = "Enabled"
   }
 }
-
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.mybucket.id
 
@@ -28,6 +26,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   }
 }
 
+# Create Dynamo DB Table for TF Backend
 resource "aws_dynamodb_table" "terraform_state" {
   name         = var.project_name + local.name_suffix
   billing_mode = "PAY_PER_REQUEST"
